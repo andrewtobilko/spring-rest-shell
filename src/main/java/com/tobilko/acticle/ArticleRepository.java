d34cd31c3e8af615516eddff51d46ee3c663859b
Andrew Tobilko <andrew.tobilko@gmail.com>
2017-10-30 02:21:56 +0200

    findByTitle

--- src/main/java/com/tobilko/acticle/ArticleRepository.java
+++ src/main/java/com/tobilko/acticle/ArticleRepository.java
@@ -1,8 +1,21 @@
 package com.tobilko.acticle;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.Optional;
 
 /**
  * Created by Andrew Tobilko on 10/28/17.
  */
-public interface ArticleRepository extends CrudRepository<Article, Long> {}
+@RepositoryRestResource(
+        path = "articles",
+        collectionResourceRel = "articles",
+        itemResourceRel = "article"
+)
+public interface ArticleRepository extends CrudRepository<Article, Long> {
+
+    Optional<Article> findByTitle(@Param("title") String title);
+
+}
