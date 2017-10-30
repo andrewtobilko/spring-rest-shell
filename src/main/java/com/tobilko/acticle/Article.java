d6930c0b4740504ef2eb8de375933f32724f8e77
Andrew Tobilko <andrew.tobilko@gmail.com>
2017-10-30 15:11:44 +0200

    setters

--- src/main/java/com/tobilko/acticle/Article.java
+++ src/main/java/com/tobilko/acticle/Article.java
@@ -20,12 +20,24 @@ public final class Article {
         return id;
     }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public String getTitle() {
         return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
     }
 
     public String getContent() {
         return content;
     }
-    
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
 }
